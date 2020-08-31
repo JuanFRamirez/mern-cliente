817939000f1b6785466eacc3bd82e28fd401e270
Juan Francisco Ramirez <62062374+JuanFRamirez@users.noreply.github.com>
2020-08-31 15:57:45 -0400

    Add files via upload

---
+++ build/precache-manifest.4ff03be1de1879ce84086ae0c9e85506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640bb5961861766390bbc52b91ddbaf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e3beaa696c45f8cc41",
+    "url": "/static/css/main.87903c51.chunk.css"
+  },
+  {
+    "revision": "b062647e44248252ad2f",
+    "url": "/static/js/2.b429ebc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b429ebc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e3beaa696c45f8cc41",
+    "url": "/static/js/main.d1f42975.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
